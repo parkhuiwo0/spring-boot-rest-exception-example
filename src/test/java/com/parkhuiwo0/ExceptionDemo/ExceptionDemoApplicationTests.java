e02127b084cf2b12c47fc5a437309298b36c0389
HuiwooPark <huiwo0@naver.com>
2020-06-23 17:41:11 +0900

    init : 프로젝트 초기 세팅

---
+++ src/test/java/com/parkhuiwo0/ExceptionDemo/ExceptionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkhuiwo0.ExceptionDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
