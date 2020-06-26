dbc7c823efc61c69d11ed26c9477e60ae9d7e846
HuiwooPark <huiwo0@naver.com>
2020-06-26 17:52:42 +0900

    ErrorResponse 클래스 정의

--- src/main/java/com/parkhuiwo0/ExceptionDemo/api/ErrorResponse.java
+++ src/main/java/com/parkhuiwo0/ExceptionDemo/api/ErrorResponse.java
@@ -1,5 +1,30 @@
 package com.parkhuiwo0.ExceptionDemo.api;
 
-public class ErrorResponse {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.parkhuiwo0.ExceptionDemo.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+    private LocalDateTime timestamp;
+
+    private int status;
+
+    private String code;
+
+    private String message;
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.timestamp = LocalDateTime.now();
+        this.status = errorCode.getStatus();
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+    }
 }
